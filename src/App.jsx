dffbe1699d184b1b2844421231fa96852b20e9e9
Joshua George <horheyinc8@gmail.com>
2023-05-05 03:23:32 +0200

    winning feature working

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [board, setBoard] = useState(null);
+  const [board, setBoard] = useState([]);
   const [xTurn, setXTurn] = useState(true);
+  const [xWon, setXWon] = useState(false);
+  const [oWon, setOWon] = useState(false);
   useEffect(() => {
     const firstBoard = [];
     let arr = [];
@@ -16,11 +18,24 @@ function App() {
     }
     setBoard(firstBoard);
   }, []);
+  useEffect(() => {
+    if (!xTurn) {
+      let didXWin = checkIfSomeoneWon("X");
+      if (didXWin) {
+        setXWon(true);
+      }
+    } else {
+      let didOWin = checkIfSomeoneWon("O");
+      if (didOWin) {
+        setOWon(true);
+      }
+    }
+  }, [xTurn]);
 
   const handleClick = (e) => {
     let squareId = e.target.id;
     let copyBoard = JSON.parse(JSON.stringify(board));
-    copyBoard.map((row, i) => {
+    copyBoard.map((row) => {
       row.map((square) => {
         if (square.index == squareId) {
           if (xTurn) {
@@ -35,13 +50,78 @@ function App() {
     setXTurn(!xTurn);
   };
 
+  const checkRows = (element) => {
+    let won = false;
+    board.forEach((row) => {
+      let winning = 0;
+      row.forEach((e) => {
+        console.log("here", e, element);
+        if (e.player === element) {
+          winning++;
+        }
+      });
+      if (winning === 3) {
+        won = true;
+      }
+    });
+    return won;
+  };
+
+  const checkCols = (element) => {
+    let won = false;
+    let winning = {};
+    board.forEach((col) => {
+      col.forEach((e, i2) => {
+        if (e.player === element) {
+          winning[i2] = winning[i2] + 1 || 1;
+        }
+      });
+    });
+    for (let index in winning) {
+      if (winning[index] === 3) {
+        console.log(`${element} won on column ${+index + 1}`);
+        return true;
+      }
+    }
+    return won;
+  };
+
+  const checkDiagonals = (element) => {
+    if (
+      (board[0][0].player === element &&
+        board[1][1].player === element &&
+        board[2][2].player) ||
+      (board[0][2].player === element &&
+        board[1][1].player === element &&
+        board[2][0].player === element)
+    ) {
+      console.log(`${element} won on a diagonal`);
+      return true;
+    }
+    return false;
+  };
+
+  const checkIfSomeoneWon = (element) => {
+    if (board.length === 3 && board[0][0].player) {
+      let rows = checkRows(element);
+      let cols = checkCols(element);
+      let diags = checkDiagonals(element);
+
+      console.log(rows, cols, diags);
+      if (rows || cols || diags) {
+        console.log(`${element} won!`);
+        return true;
+      }
+    }
+  };
+
   return (
     <div>
       <h1 className="heading">Tic-Tac-Tron</h1>
       <div id="board-container">
         {board &&
-          board.map((row, rowIndex) => {
-            return row.map((square, index) => {
+          board.map((row) => {
+            return row.map((square) => {
               return (
                 <div
                   key={uuidv4()}
